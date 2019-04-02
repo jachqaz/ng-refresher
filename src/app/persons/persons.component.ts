abd0a9e0865ed32c0f68b7fd05b420a6e9f2db59
Johan <jach_1997@hotmail.com>
2019-04-01 21:53:26 -0500

    Cross Component

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-persons',
@@ -6,11 +6,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./persons.component.css']
 })
 export class PersonsComponent implements OnInit {
+  @Input() personList: string[];
 
   constructor() {
   }
 
   ngOnInit() {
   }
+
 
 }
