98f9518492824f368c04ba10160b1fb4295e8aa3
Johan <jach_1997@hotmail.com>
2019-04-07 19:49:06 -0500

    Implementing Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {PersonsComponent} from './persons/persons.component';
 import {PersonInputComponent} from './persons/person-input/person-input.component';
 import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {FormsModule} from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
